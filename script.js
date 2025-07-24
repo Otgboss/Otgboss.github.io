f8d8ad5058a52f216ebdac217a7579095d95ced5
OTG BOSS <felixmuchiri9@gmail.com>
2025-07-24 15:47:57 +0300

    Enhanced change text with a button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("OTG is live");
 const button = document.getElementById("join-btn");
 button.addEventListener("click",function (){
-    alert("Welcome to OTG. Open Threshold Ghakime!");
+    alert("Welcome to OTG. Omni-Task Ghakime!");
 });
+function changeText() {
+    document.getElementById("change-text").innerText = "The text has been changed";
+}
