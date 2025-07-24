0d428aea64b01529b4e78b1033f25c423bc157a6
OTG BOSS <felixmuchiri9@gmail.com>
2025-07-24 14:38:29 +0300

    Made the 'Join The Movement' button responsive

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("OTG is live");
+const button = document.getElementById("join-btn");
+button.addEventListener("click",function (){
+    alert("Welcome to OTG. Open Threshold Ghakime!");
+});
