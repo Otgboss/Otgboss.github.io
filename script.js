63328c539f15f56a80f8c4a3474a498fc37377ef
OTG BOSS <felixmuchiri9@gmail.com>
2025-08-04 17:57:34 +0300

    made the scrollToTopBtn interactive

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const button = document.getElementById("join-btn");
 button.addEventListener("click",function (){
     alert("Welcome to OTG. Omni-Task Ghakime!");
 });
+//Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+//Show the button when scrolling down
+window.onscroll = function(){
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        scrollToTopBtn.style.display = "block";
+    } else{
+        scrollToTopBtn.style.display = "none";
+    }
+};
+//Scroll to top when clicked
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
