959a713028f72ffdcf921343cc8da1b878c02fd6
OTG BOSS <felixmuchiri9@gmail.com>
2025-08-04 18:50:12 +0300

    edited the scrollToTopBtn

--- script.js
+++ script.js
@@ -4,20 +4,16 @@ button.addEventListener("click",function (){
     alert("Welcome to OTG. Omni-Task Ghakime!");
 });
 //Get the button
-const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-if (scrollToTopBtn) {
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 window.onscroll = function(){
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
         scrollToTopBtn.style.display = "block";
     } else{
         scrollToTopBtn.style.display = "none";
     }
 };
 //Scroll to top when clicked
-scrollToTopBtn.addEventListener("click", () => {
+scrollToTopBtn.addEventListener("click", function () {
     window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+        top: 0, behavior: "smooth"});
 });
-}
