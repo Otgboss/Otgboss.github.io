a29f7e450aa3ef5ee8c17a8194e906c4d2d32ffa
OTG BOSS <felixmuchiri9@gmail.com>
2025-08-04 17:03:43 +0300

    removed the change-text button

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ const button = document.getElementById("join-btn");
 button.addEventListener("click",function (){
     alert("Welcome to OTG. Omni-Task Ghakime!");
 });
-const changeBtn= document.getElementById("change-btn");
-changeBtn.addEventListener("click",function (){
-    document.getElementById("change-text").textContent="The text has been changed";
-});
