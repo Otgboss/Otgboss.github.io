d2041544a077c5f7b2a9a52618e27d1410ef812c
OTG BOSS <felixmuchiri9@gmail.com>
2025-11-19 22:27:47 +0300

    made changes to the app.py file

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ app = Flask(__name__)
 @app.route('/')
 def home ():
     return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 if __name__ == '__main__':
     app.run(debug=True)
