8763a60377c2865ba27aa5643a6a3b6cacdb80b3
Felix <felixmuchiri9@gmail.com>
2025-12-22 23:47:23 +0300

    Add user model and set-up Flask-migrate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 app = Flask(__name__)
 #Data configurations
@@ -9,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from models import ContactMessage, User #Important: this registers the models
 @app.route('/')
 def home ():
     return render_template('index.html')
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from app import db
 from datetime import datetime, timezone
-class ContactMessage(db.model):
+class ContactMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False)
@@ -8,3 +8,12 @@ class ContactMessage(db.model):
     created_at = datetime.now(timezone.utc)
     def __repr__(self):
         return f"<ContactMessage{self.email}>"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80),unique=True, nullable=False)
+    email = db.Column(db.String(120),unique=True, nullable=False)
+    password = db.Column(db.String(200),nullable=False)
+    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+    def __repr__(self):
+        return f'<User {self.username}>'
